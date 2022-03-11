71fc3c2b47517a7474ec4a467b5cee0c707d70b3
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2022-03-11 19:22:16 +0000

    slider test+

--- src/Contents/Slider.js
+++ src/Contents/Slider.js
@@ -1,8 +1,6 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from 'swiper';
 import "./CSS/swiper.min.css";
-//import "./components/effect-coverflow/effect-coverflow.min.css"
-//import "./components/pagination/pagination.min.css";
-//import "./components/navigation/navigation.min.css";
 import "./CSS/slider.css"
 import SwiperCore, {
     EffectCoverflow,
@@ -10,41 +8,43 @@ import SwiperCore, {
     Navigation
 } from "swiper/core";
 
-SwiperCore.use([EffectCoverflow, Pagination, Navigation]);
-
 
 const Slider = () => {
-
+    SwiperCore.use([Autoplay])
+    SwiperCore.use([EffectCoverflow, Pagination, Navigation]);
     return (
-
+        
         <div className="slider-body">
-            <Swiper
-            Autoplay={true}
+            <Swiper 
+            
                 navigation={true}
                 effect={"coverflow"}
                 centeredSlides={true}
                 slidesPerView={window.innerWidth < 768 ? 1 : "auto"}
                 loop={true}
+                autoplay={{
+                    delay: 2000,
+                    disableOnInteraction: false
+                }}
                 coverflowEffect={{
                     rotate: 50,
                     stretch: 0,
-                    depth: 100,
+                    depth: 400,
                     modifier: 1,
                     slideShadows: true
                 }}
                 pagination={{
                     clickable: true
                 }}
-                className="mySwiper"
             >
                 <SwiperSlide>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7-uIjwaqEu3W_q9jCNvK9r3KdCFUHGMMs_A&usqp=CAU" alt="img1" />
+                    <img className="slider"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7-uIjwaqEu3W_q9jCNvK9r3KdCFUHGMMs_A&usqp=CAU" alt="img1"/>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiYapAPWUBa3MWsJRxDlGg4l9p9uHOf8CS_g&usqp=CAU" alt="img2"/>
+                    <img className="slider"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiYapAPWUBa3MWsJRxDlGg4l9p9uHOf8CS_g&usqp=CAU" alt="img2"/>
                 </SwiperSlide>
-                <SwiperSlide>
-                    <img src="https://st1.photogallery.ind.sh/wp-content/uploads/indiacom/sunny-leone-bikini-photo-201610-1585208358.jpg" alt="img3"/>
+                <SwiperSlide >
+                    <img className="slider" src="https://st1.photogallery.ind.sh/wp-content/uploads/indiacom/sunny-leone-bikini-photo-201610-1585208358.jpg" alt="img3"/>
                 </SwiperSlide>
             </Swiper>
         </div>
