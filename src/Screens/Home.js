97e0c44111fbdfe631db7695c6968294c8f70943
Sazzad Hossain <sazzad15-2521@diu.edu.bd>
2022-03-12 01:16:15 +0600

    ok

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -10,50 +10,54 @@ import './Css/Home.css'
 const Home = () => {
 
     return (
-        <Parallax pages={3} style={{ top: '0', left: '0' }}>
-            <ParallaxLayer
-                offset={0}
-                speed={2.5}
-                style={{ display: 'inline-block', justifyContent: 'center' }}>
-                <Header />
-                <div style={{
-                    display: 'flex',
-                    flexWrap: 'wrap',
-                    width: '100%',
-                }}>
-                    <div className='home-div'>
-                    <UserInfo/>
+        <div>
+            <Header />
+            <Parallax pages={3} style={{ top: '0', left: '0' }}>
+                <ParallaxLayer
+                    offset={0}
+                    speed={2.5}
+                    style={{ display: 'inline-block', justifyContent: 'center' }}>
+                    <div style={{
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        width: '100%',
+                        marginTop:'120px'
+                    }}>
+                        <div className='home-div'>
+                            <UserInfo />
+                        </div>
+                        <div className='home-div'>
+                            <Slider />
+                        </div>
                     </div>
-                    <div className='home-div'>
-                        <Slider />
-                    </div>
-                </div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1} speed={.5} style={{ backgroundColor: '#ff6d6d' }} />
-            <ParallaxLayer
-                offset={1}
-                speed={0.8}
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    color: 'black',
-                }}>
-                <OurProject/>
-            </ParallaxLayer>
-            <ParallaxLayer offset={2} speed={2} style={{ backgroundColor: '#ff6d6d' }} />
-            <ParallaxLayer
-                offset={2}
-                speed={0.5}
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    color: 'white',
-                }}>
-                <Footer/>
-            </ParallaxLayer>
-        </Parallax>
+                </ParallaxLayer>
+                <ParallaxLayer offset={1} speed={.5} style={{ backgroundColor: '#ff6d6d' }} />
+                <ParallaxLayer
+                    offset={1}
+                    speed={0.8}
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        color: 'black',
+                    }}>
+                    <OurProject />
+                </ParallaxLayer>
+                <ParallaxLayer offset={2} speed={2} style={{ backgroundColor: '#ff6d6d' }} />
+                <ParallaxLayer
+                    offset={2}
+                    speed={0.5}
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        color: 'white',
+                    }}>
+                    <Footer />
+                </ParallaxLayer>
+            </Parallax>
+        </div>
+
     );
 };
 
