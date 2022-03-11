3cbe4c9a4de081635da761dfd6ee9f933f821d19
Sazzad Hossain <sazzad15-2521@diu.edu.bd>
2022-03-11 18:06:18 +0600

    ok

---
+++ src/Contents/OurProject.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const OurProject = () => {
+    return (
+        <div>
+            <p style={{
+                color: 'black',
+                fontSize: '30px'
+            }}>Project page</p>
+        </div>
+    );
+};
+
+export default OurProject;
