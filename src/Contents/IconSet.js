378d3e66148248290058749f9141e589cb3e386a
amandiu <79790568+amandiu@users.noreply.github.com>
2022-03-12 17:30:23 +0600

    ok

--- src/Contents/IconSet.js
+++ src/Contents/IconSet.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import './CSS/IconSet.css';
-import {
-    Facebook,
-    Twitter,
-    Instagram,
-    YouTube
-} from '@mui/icons-material'
 
 const IconSet = () => {
-    
+
     return (
         <div>
-            <div className='f'> <Facebook style={{
-                fontSize: '40px',
-                '&:hover':{
-                    fontSize:'50px'
-                }
-            }}/></div>
-            <div className='f'><Twitter/></div>
-            <div className='f'><Instagram/></div>
-            <div className='f'><YouTube/></div>
+            
         </div>
 
     );
