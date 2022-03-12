4dedc65c0a86020621c6cfafea9ddb042d70d078
amandiu <79790568+amandiu@users.noreply.github.com>
2022-03-12 23:42:14 +0600

    ok

--- src/Contents/IconSet.js
+++ src/Contents/IconSet.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import './CSS/IconSet.css';
-import { IconContext } from "react-icons";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF, faYoutube, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
+
+
+library.add(faFacebookF);
 
 const IconSet = () => {
 
     return (
-        <IconContext.Provider value={{ color: "blue", className: "global-class-name" }}>
-            <div>
-                <FaFolder />
+        <div className='s'>
+            <div className='f'>
+                <FontAwesomeIcon  icon="fa-brands fa-facebook-f" />
             </div>
-        </IconContext.Provider>
-
-
+            <div className='f'>
+                <FontAwesomeIcon  icon={faTwitter} />
+            </div>
+            <div className='f'>
+                <FontAwesomeIcon  icon={faInstagram} />
+            </div>
+            <div className='f'>
+                <FontAwesomeIcon icon={faYoutube} />
+            </div>
+        </div>
     );
 };
 
