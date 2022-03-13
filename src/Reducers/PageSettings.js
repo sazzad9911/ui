8d7068d509200bbeceaeaa30b4fc5c411cd8a9da
Sazzad Hossain <sazzad15-2521@diu.edu.bd>
2022-03-14 01:37:34 +0600

    ok

--- src/Reducers/PageSettings.js
+++ src/Reducers/PageSettings.js
@@ -7,15 +7,18 @@ const initialState={
         state:false,
         head:'No Head',
         text:'Loading...',
-    }
+    },
+    isDark:true,
 }
 
 const PageSettings=(state=initialState, action)=> {
     switch(action.type){
         case 'LOADER':
-            return state={loader: action.playload, alert: state.alert};
+            return state={loader: action.playload, alert: state.alert,isDark:state.isDark};
         case 'ALERT':
-            return state={alert: action.playload, loader: state.loader};    
+            return state={alert: action.playload, loader: state.loader,isDark:state.isDark};  
+        case 'THEME':
+            return state={alert: state.alert, loader: state.loader,isDark:action.playload}     
         default:
             return state;
     }
