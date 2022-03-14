19dce57ab67b3f3cc2ac15458e6a968ad5e20673
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2022-03-14 08:53:30 +0000

    done

--- src/Contents/Post.js
+++ src/Contents/Post.js
@@ -6,15 +6,39 @@ import './CSS/Post.css'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import LoadingButton from '@mui/lab/LoadingButton';
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 import SendIcon from '@mui/icons-material/Send';
 import { useSelector } from "react-redux";
+import { orange, purple } from "@mui/material/colors";
 
 
 const Post = () => {
-    const theme= useSelector(state=>state.PageSettings.isDark)
-
+    const theme = useSelector(state => state.PageSettings.isDark)
+    const theme1 = createTheme({
+        palette: {
+            secondary: {
+                main: orange[500],
+            },
+            primary: {
+                main: purple[500],
+            },
+            placeholder:{
+                main: purple[500]
+            }
+        },
+        overrides: {
+            MuiInput: {
+              input: {
+                "&::placeholder": {
+                  color: "white"
+                },
+                color: "white", // if you also want to change the color of the input, this is the prop you'd use
+              }
+            }
+        }
+    })
     const settings = {
         dots: false,
         infinite: true,
@@ -29,94 +53,110 @@ const Post = () => {
 
 
     return (
-        <div className="container">
-            <h1>Post and Comments</h1>
-            <Slider {...settings}>
-                <div className="slide">
-                    <div className="slide-left">
-                        <img height={300} width={300} src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png' />
-                    </div>
-                    <div className="slide-right">
-                        <h3 style={{
-                            color: theme?'white':'black'
-                        }}>What is React Native?</h3>
-                        <p>React Native combines the best parts of native development with React, a best-in-class JavaScript library for building user interfaces.</p>
-                        <div className="comment">
-                            <Box
-                                component="form"
-                                sx={{
-                                    '& .MuiTextField-root': { m: 1, width: '25ch' },
-                                }}
-                                noValidate
-                                autoComplete="off"
-                            >
-                                <div>
-                                    <TextField
-                                        id="outlined-multiline-static"
-                                        label="Comment"
-                                        multiline
-                                        rows={4}
-                                        placeholder="Enter Your Any Comments!"
-                                    />
-                                </div>
-                                <div className="send-button">
-                                    <LoadingButton
-                                        size="small"
-                                        endIcon={<SendIcon />}
-                                        loadingPosition="end"
-                                        variant="outlined"
-                                    >
-                                        Send
-                                    </LoadingButton>
-                                </div>
-                            </Box>
+        <ThemeProvider theme={theme1}>
+            <div className="container">
+                <h1 style={{
+                    color: theme ? 'white' : 'black'
+                }}>Post and Comments</h1>
+                <Slider {...settings}>
+                    <div className="slide">
+                        <div className="slide-left">
+                            <img height={300} width={300} src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png' />
                         </div>
+                        <div className="slide-right">
+                            <h3 style={{
+                                color: theme ? 'white' : 'black'
+                            }}>What is React Native?</h3>
+                            <p style={{
+                                color: theme ? 'white' : 'black'
+                            }}>React Native combines the best parts of native development with React, a best-in-class JavaScript library for building user interfaces.</p>
+                            <div className="comment">
+                                <Box
+                                    component="form"
+                                    sx={{
+                                        '& .MuiTextField-root': { m: 1, width: '25ch' },
+                                    }}
+                                    noValidate
+                                    autoComplete="off"
+                                >
+                                    <div>
+                                        <TextField 
+                                            className="textfield"
+                                            id="outlined-multiline-static"
+                                            label="Comment"
+                                            sx={{"label": {color: theme ? 'white' : 'black'}}}
 
-                    </div>
-                </div >
-                <div className="slide">
-                    <div className="slide-left">
-                        <img height={300} width={300} src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png' />
-                    </div>
-                    <div className="slide-right">
-                        <h3>What is React Native?</h3>
-                        <p>React Native combines the best parts of native development with React, a best-in-class JavaScript library for building user interfaces.</p>
-                        <div className="comment">
-                            <Box
-                                component="form"
-                                sx={{
-                                    '& .MuiTextField-root': { m: 1, width: '25ch' },
-                                }}
-                                noValidate
-                                autoComplete="off"
-                            >
-                                <div>
-                                    <TextField
-                                        id="outlined-multiline-static"
-                                        label="Comment"
-                                        multiline
-                                        rows={4}
-                                        placeholder="Enter Your Any Comments!"
-                                    />
-                                </div>
-                                <div className="send-button">
-                                    <LoadingButton
-                                        size="small"
-                                        endIcon={<SendIcon />}
-                                        loadingPosition="end"
-                                        variant="outlined"
-                                    >
-                                        Send
-                                    </LoadingButton>
-                                </div>
-                            </Box>
+                                            multiline
+                                            rows={4}
+                                        />
+                                    </div>
+                                    <div className="send-button">
+                                        <LoadingButton
+                                            size="small"
+                                            endIcon={<SendIcon />}
+                                            loadingPosition="end"
+                                            variant="outlined"
+                                        >
+                                            <p style={{
+                                                color: theme ? 'white' : 'black'
+                                            }}>Send</p>
+                                        </LoadingButton>
+                                    </div>
+                                </Box>
+                            </div>
+
                         </div>
+                    </div >
+                    <div className="slide">
+                        <div className="slide-left">
+                            <img height={300} width={300} src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png' />
+                        </div>
+                        <div className="slide-right">
+                            <h3 style={{
+                                color: theme ? 'white' : 'black'
+                            }}>What is React Native?</h3>
+                            <p style={{
+                                color: theme ? 'white' : 'black'
+                            }}>React Native combines the best parts of native development with React, a best-in-class JavaScript library for building user interfaces.</p>
+                            <div className="comment">
+                                <Box
+                                    component="form"
+                                    sx={{
+                                        '& .MuiTextField-root': { m: 1, width: '25ch' },
+                                    }}
+                                    noValidate
+                                    autoComplete="off"
+                                >
+                                    <div>
+                                    <TextField
+                                            id="outlined-multiline-static"
+                                            label="Comment"
+                                            sx={{"label": {color: theme ? 'white' : 'black'}}}
+                                            multiline
+                                            rows={4}
+                                        />
+                                    </div>
+                                    <div className="send-button">
+                                        <LoadingButton
+                                            size="small"
+                                            endIcon={<SendIcon />}
+                                            loadingPosition="end"
+                                            variant="outlined"
+                                        >
+                                            <p style={{
+                                                color: theme ? 'white' : 'black'
+                                            }}>Send</p>
+                                        </LoadingButton>
+                                    </div>
+                                </Box>
+                            </div>
 
-                    </div>
-                </div >
-            </Slider >
-            
-        </div >
+                        </div>
+                    </div >
+                </Slider >
+
+            </div >
+        </ThemeProvider>
     );
 }
 
