0cdacbd87dba91adffb31fecd395f6973cb0889d
Sazzad Hossain <sazzad15-2521@diu.edu.bd>
2022-03-14 13:33:06 +0600

    ok

--- src/Contents/UserInfo.js
+++ src/Contents/UserInfo.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {useSelector} from 'react-redux'
 
 const UserInfo = () => {
+    const theme= useSelector(state=>state.PageSettings.isDark)
+
     return (
         <div>
-            <h1 style={{color: 'white'}}>Hi, Md Shariar</h1>
+            <h1 style={{color: theme? 'white':'black'}}>Hi, Md Shariar</h1>
         </div>
     );
 };
