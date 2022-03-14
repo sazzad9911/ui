908c224e198333e08a436859dff7393db394c303
amandiu <79790568+amandiu@users.noreply.github.com>
2022-03-14 20:18:55 +0600

    ok

--- src/Contents/Footer.js
+++ src/Contents/Footer.js
@@ -7,38 +7,30 @@ import { faFacebookF, faYoutube, faTwitter, faInstagram } from '@fortawesome/fre
 const Footer = () => {
     return (
         <div className='footerbody'>
-            <div className='footerbodyf'>
-                <div className='footerscientistxicon'>
-                    <img src={png} height="120px" width="auto" />
+            <div className='footerbodyone'>
+                <div className='bodyf'>
+                <div className='bodyftop'></div>
+                <div><img src={png} height="80px" width="auto" /></div>
+                <div> <p>This is the site for something new innovation.You can build with us any project.</p></div>
+                <div className='ficon'>
+                <div className='footerIconSetf'><FontAwesomeIcon icon="fa-brands fa-facebook-f" /></div>
+                <div className='footerIconSetf'><FontAwesomeIcon icon={faTwitter} /></div>
+                <div className='footerIconSetf'><FontAwesomeIcon icon={faInstagram} /></div>
+                <div className='footerIconSetf'><FontAwesomeIcon icon={faYoutube} /></div>
+                </div>                
                 </div>
-                <div className='footertxt'>
-                    <p>This is the site for something new innovation.You can build with us any project.</p>
-                </div>
-                <div className='footerIconSetbox'>
-                    <div className='footerIconSets'>
-                        <div className='footerIconSetf'>
-                            <FontAwesomeIcon icon="fa-brands fa-facebook-f" />
-                        </div>
-                        <div className='footerIconSetf'>
-                            <FontAwesomeIcon icon={faTwitter} />
-                        </div>
-                        <div className='footerIconSetf'>
-                            <FontAwesomeIcon icon={faInstagram} />
-                        </div>
-                        <div className='footerIconSetf'>
-                            <FontAwesomeIcon icon={faYoutube} />
-                        </div>
-                    </div>
+                <div className='bodys'>
+                <p>ABOUT<br />SERVICES<br />JOB OPPORTUNITIES<br />CALENDER OF EVENTS<br />LOCATION</p>
                 </div>
             </div>
-            <div className='footerbodys'>
-            <p className='footerbodystxt'>ABOUT<br/>SERVICES<br/>JOB OPPORTUNITIES<br/>CALENDER OF EVENTS<br/>LOCATION</p>
-            </div>
-            <div className='footerbodyt'>
+            <div className='footerbodytwo'>
+                <div className='bodyth'>
 
-            </div>
-            <div className='footerbodyfo'>
-
+                </div>
+                <div className='bodyfo'>
+                <p>amanullah</p>
+                </div>
+                
             </div>
         </div>
     );
